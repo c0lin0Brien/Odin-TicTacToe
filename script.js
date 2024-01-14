96fecd3ac72a586553a9c859a9383a06fd6a47ac
c0lin0Brien <colinpobrien04@gmail.com>
2024-01-13 17:21:09 -0800

    Implemented userMove and oppMove functions

--- script.js
+++ script.js
@@ -13,8 +13,13 @@ const player = function(token) {
 const game = (function() {
     let board = gameBoard.board;
     let win = false;
-    const p1 = player("X");
-    const p2 = player("O");
+    const initGame = function (selection, other) {
+        let user = player(selection);
+        let opp = player(other);
+        return [user, opp];
+    }
+    // For testing purposes we'll keep it like this
+    const [user, opp] = initGame("X", "O");
     const winCheck = function () {
         for (i=0; i<3; i++) {
             // Check rows and columns for 3 in a row
@@ -25,10 +30,30 @@ const game = (function() {
             // Check diagonals for a win
             } else if (board[4] != 0 && ((board[0] == board[4] && board[4] == board[8]) ||
                         (board[2] == board[4] && board[4] == board[6]))){
-                win = true;
+                game.win = true;
                 console.log(`${board[4]} has won`);
             }
         }
     }
-    return {board, win, p1, p2, winCheck};
+    const userMove = function(tile) {
+        if (board[tile] == 0) {
+            board[tile] = user.token;
+            // Player moves:
+            console.log("Player moved", game.board);
+            winCheck();
+            // Opponent moves:
+            oppMove();
+        }
+    }
+    const oppMove = function() {
+        let choice = Math.floor(Math.random() * 9);
+        if (board[choice] == 0) {
+            board[choice] = opp.token;
+            console.log("Opponent moved", game.board);
+            winCheck();
+        } else {
+            oppMove();
+        }
+    }
+    return {board, win, winCheck, userMove, oppMove, initGame, user, opp};
 })();
