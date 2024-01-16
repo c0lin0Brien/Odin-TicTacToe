758715f2f6b22a70ded893d486e105af001f94c2
c0lin0Brien <colinpobrien04@gmail.com>
2024-01-15 18:26:30 -0800

    Horizontal wins now register

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// TO DO:
+// - Register Horizontal wins
+// 
 // Making the Game Board
 const gameBoard = (function() {
     let board = ["", "", "",
@@ -43,14 +46,14 @@ const game = (function() {
     const winCheck = function () {
         for (i=0; i<3; i++) {
             // Check rows and columns for 3 in a row
-            if (board[i] != "" && (board[i] == board[i+1] && board[i+1] == board[i+2] ||
-                board[i] == board[i+3] && board[i+3] == board[i+6])) {
-                    console.log(`${board[i]} has won`);
+            if ((board[i * 3] != "" && (board[i * 3] == board[(i * 3) + 1] && board[(i * 3) + 1] == board[(i * 3) + 2])) ||
+                (board[i] != "" && (board[i] == board[i+3] && board[i+3] == board[i+6]))) {
+                    console.log(`${user.token} has won`);
                     return true;
             // Check diagonals for a win
             } else if (board[4] != "" && ((board[0] == board[4] && board[4] == board[8]) ||
                         (board[2] == board[4] && board[4] == board[6]))){
-                console.log(`${board[4]} has won`);
+                console.log(`${user.token} has won`);
                 return true;
             }
         }
